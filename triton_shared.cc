0614763d270ec0eacba9d5d8283cdff6bedb03c8
Nhat Nguyen <nhat@meta.com>
2026-02-23 14:11:41 -0500

    Fork at commit 08684f9

---
+++ triton_shared.cc
@@ -0,0 +1,15 @@
+﻿//===----------------------------------------------------------------------===//
+//
+// Copyright (c) Meta Platforms, Inc. and affiliates, Microsoft Corporation.
+// Licensed under the MIT license.
+//
+//===----------------------------------------------------------------------===//
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+// The CPU backend with triton_shared doesn't do compilation from within python
+// but rather externally through triton-shared-opt, so we leave this function
+// blank.
+void init_triton_triton_shared(py::module &&m) {}
